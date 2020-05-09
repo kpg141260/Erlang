6e06076dbafa233dfd85b101e3d653f9d4264c32
Peter Gossler <kpg141260@live.de>
2020-05-09 18:23:32 +0800

    Formatting Changes to Screen Output
    Finalised formatting of screen output changes.

--- erlang_test.py
+++ erlang_test.py
@@ -19,9 +19,18 @@ ec = erlang_c.Erlang(0.80, 30, 300, 40, 20, 30, False, 1, 60, 16)
 
 service_time = 30
 call_data = [["08:00", 100], ["09:00", 120], ["10:00", 168], ["11:00", 236], ["12:00", 331], ["13:00", 464], ["14:00", 650], ["15:00", 910], ["16:00", 546], ["17:00", 328], ["18:00", 197], ["19:00", 119], ["20:00", 72], ["21:00", 44], ["22:00", 27], ["23:00", 17]]
+# The header for the grid output - this we will also use later to export into a csv file
+csv_header = ["Time", "Calls", "Agents", "Util", "SLA","ASA","Abandon","Q-Percent","Q-Time","Q-Count"]
 max_call_vol = 0
 idx = 0
 count = 0
+# Adjust rjust value to number of spaces
+adjust_val = 11
+# A string that we need to out put to the terminal and csv header
+str_head = ''
+# A temporary string needed
+tmp = ''
+
 for x in call_data:
     if x[1] > max_call_vol:  
         max_call_vol = x[1]
@@ -47,19 +56,43 @@ print ("Queue Time      {}sec".format(ec.QueueTime(agents, max_call_vol)))
 print ("Queue Size      {}".format(ec.QueueSize(agents, max_call_vol)))
 
 print ("\nCalculating data set...\n")
-print ("Time   Agents  Utilisation SLA       ASA     Abandoned   Queued    Queue-Time  Queue-Size")
-print ("-----------------------------------------------------------------------------------------")
+# Format the header string for output - will be reused further down
+for Item in csv_header:
+    tmp = format(Item.ljust(adjust_val))
+    str_head = str_head + tmp
+# Print the header
+print(str_head)
+# print a line of dashes
+print ("-" * len(str_head))
+# Clear temp string
+tmp = ''
+str_data = ''
 for x in call_data:
-    tm = x[0]
+    tmp = str(x[0]).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(x[1]).ljust(adjust_val)
+    str_data = str_data + tmp
     ag = ec.Agents (service_time, x[1])
-    ut = ec.Utilisation(ag, x[1]) * 100
-    sla = ec.SLA(ag, x[1], service_time) * 100
-    asa = ec.ASA(ag, x[1])
-    abn = ec.Abandon(ag, x[1]) * 100
-    qc = ec.Queued(ag, x[1]) * 100
-    qt = ec.QueueTime(ag, x[1])
-    qs = ec.QueueSize(ag, x[1])
-    print ("{}  {}      {:0.2f}%      {:0.2f}%    {:0}sec   {:0.2f}%      {:0.2f}%    {}          {}".format(tm, ag, ut, sla, asa, abn, qc, qt, qs))
+    tmp = str(ag).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.Utilisation(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.SLA(ag, x[1], service_time), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.ASA(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round (ec.Abandon(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.Queued(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.QueueTime(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.QueueSize(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    print (str_data)
+    # Clear string objects for next loop
+    str_data = ''
+    tmp = ''
 print ()
 
 del ec
@@ -110,17 +143,41 @@ print ("Queue Time      {}sec".format(ec.QueueTime(agents, max_call_vol)))
 print ("Queue Size      {}".format(ec.QueueSize(agents, max_call_vol)))
 
 print ("\nCalculating data set...\n")
-print ("Time   Agents  Utilisation SLA       ASA     Abandoned   Queued    Queue-Time  Queue-Size")
-print ("-----------------------------------------------------------------------------------------")
+
+# Print the header
+print(str_head)
+# print a line of dashes
+print ("-" * len(str_head))
+
 for x in call_data:
-    tm = x[0]
+    tmp = str(x[0]).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(x[1]).ljust(adjust_val)
+    str_data = str_data + tmp
     ag = ec.Agents (service_time, x[1])
-    ut = ec.Utilisation(ag, x[1]) * 100
-    sla = ec.SLA(ag, x[1], service_time) * 100
-    asa = ec.ASA(ag, x[1])
-    abn = ec.Abandon(ag, x[1]) * 100
-    qc = ec.Queued(ag, x[1]) * 100
-    qt = ec.QueueTime(ag, x[1])
-    qs = ec.QueueSize(ag, x[1])
-    print ("{}  {}      {:0.2f}%      {:0.2f}%    {:0}sec   {:0.2f}%      {:0.2f}%    {}          {}".format(tm, ag, ut, sla, asa, abn, qc, qt, qs))
+    tmp = str(ag).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.Utilisation(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.SLA(ag, x[1], service_time), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.ASA(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round (ec.Abandon(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(round(ec.Queued(ag, x[1]), 2)).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.QueueTime(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    tmp = str(ec.QueueSize(ag, x[1])).ljust(adjust_val)
+    str_data = str_data + tmp
+    print (str_data)
+    # Clear string objects for next loop
+    str_data = ''
+    tmp = ''
 print ()
+
+del ec
+del call_data
+del str_data
+del tmp
